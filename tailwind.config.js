646521850c7ac1cef4b21b9e0215120b6ef3a9c6
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2024-11-05 22:40:57 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    screens: {
+      '2xl': { 'max': '1440px' },
+      // => @media (max-width: 1440px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '991px' },
+      // => @media (max-width: 991px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '479px' },
+      // => @media (max-width: 479px) { ... }
+    },
   },
   plugins: [],
 }
